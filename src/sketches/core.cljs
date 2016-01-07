92258755339abd6de1acda82e34236c42e10e8c0
Naji Mammeri <naji@pascience.net>
2016-01-07 04:28:36 +0100

    deck of cards

--- src/sketches/core.cljs
+++ src/sketches/core.cljs
@@ -4,11 +4,11 @@
 
 (defn setup []
   (q/frame-rate 30)
-  (q/color-mode :hsb)
+  (q/color-mode :hsv)
   {:nbp 50})
 
 (defn update-state [state]
-  {:nbp 60
+  {:nbp 80
    :phase 2})
 
 ; (def t q/with-translation)
@@ -20,21 +20,31 @@
 
 (defn square [x] (* x x))
 
-(def canvas-width 500)
+(def canvas-side 500)
+(def canvas-width canvas-side)
+(def canvas-height canvas-side)
+(def canvas-dim [canvas-width canvas-height])
 
 (defn *sqrt2 [x] (* (.sqrt js/Math 2) x))
+(defn sqrt [x] (.sqrt js/Math x))
+
+(def w canvas-width)
+(def h canvas-height)
 
 (defn draw-state [{:keys [nbp]}]
   (q/background 0)
-  (doseq [p (range 0 (inc nbp))]
-      (let [step (/ (*sqrt2 canvas-width) nbp)
+  (doseq [point (range 0 (inc nbp))]
+      (let [p (/ point nbp)
             f (q/frame-count)
-            x (* step p)
-            y (* step p)
-            phase (/ f 7)
-            color (lin-scale 0 (/ p nbp) 360)
-            size (lin-scale 60 (q/sin (+ p phase)) 100)]
-        (q/fill color 255 255)
+            pos1 (fn [p min max] (lin-scale min p max))
+            x (pos1 p (* 0.40 w) (* 0.85 w))
+            y (pos1 p (* 0.25 h) (* 0.75 h))
+            phase (/ f 25)
+            hue (lin-scale 0 p 360)
+            saturation (lin-scale 250 p 0)
+            value (lin-scale 0 p 360)
+            size 60]
+        (q/fill hue saturation value)
         (q/rect (- x (/ size 2)) (- y (/ size 2)) size size))))
   
 ;  (q/fill (:color state) 255 255)
